4f37072043a7d8f95357fbcdcc7e035f5837f402
tingxin <friendship-119@163.com>
2025-04-28 04:58:27 +0000

    update

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 
 import os
+import argparse
 from tools import conf, unload
 
-conf.load_env()
+def main():
+    parser = argparse.ArgumentParser(description='MaxCompute data migration tool')
+    parser.add_argument('--job-name', '-j', 
+                      type=str, 
+                      default='default_job',
+                      help='Name of the unload job')
+    
+    args = parser.parse_args()
+    
+    # Load environment variables
+    conf.load_env()
+    
+    # Get table names from environment
+    table_names = os.getenv("TABLE_NAME").split(",")
+    
+    # Run the unload job with specified job name
+    unload.run(args.job_name, table_names)
 
-table_names = os.getenv("TABLE_NAME").split(",")
-
-unload.run("test1", table_names)
+if __name__ == "__main__":
+    main()
